ad8efa00d5572bbc861f19d9c56c2b03b64e4944
Dhouib Mohamed <mohamed.dhouib@insat.ucar.tn>
2023-07-09 14:13:42 +0100

    updating eslintrc

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -25,7 +25,28 @@ module.exports = {
 			parserOptions: {
 				parser: '@typescript-eslint/parser'
 			},
-			rules: 'off'
+			rules: {
+				'globalThis': 'off',
+				'@typescript-eslint/no-empty-function': 'off',
+				'no-undef': 'off',
+				'@typescript-eslint/no-this-alias': 'off',
+				'no-empty': 'off',
+				'no-case-declarations': 'off',
+				'no-self-assign': 'off',
+				'no-empty-function': 'off',
+				'no-useless-escape': 'off',
+				'no-redeclare': 'off',
+				'no-prototype-builtins': 'off',
+				'no-control-regex': 'off',
+				'@typescript-eslint/ban-ts-comment': 'off',
+				'no-var-requires': 'off',
+				'no-unused-vars': 'off',
+				'no-irregular-whitespace': 'off',
+				'no-async-promise-executor': 'off',
+				'no-empty-method': 'off',
+				'no-constant-condition': 'off',
+				'@typescript-eslint/no-unused-vars': 'off'
+			}
 		}
 	]
 };
